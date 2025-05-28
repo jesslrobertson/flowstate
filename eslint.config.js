9ff6592b8bf6199368bcee49bcd48c681c20764f
Jess Robertson <jess.robertson@portlandwebworks.com>
2025-05-28 11:35:58 -0700

    Add lint and formatting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,28 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
+const prettierPlugin = require('eslint-plugin-prettier')
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
+
   {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      'prettier/prettier': 'warn',
+      'react-native/no-inline-styles': 'warn',
+      'react/jsx-boolean-value': ['warn', 'always'],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+
     ignores: ['dist/*'],
   },
-]);
+])
